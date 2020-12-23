039071735c58c442ecd659d280d4b162d6280f43
jh.kim <jh.kim@impix.co.kr>
2020-12-23 15:52:07 +0900

    viemo.svg 이미지 변경, youtube.svg 삭제
    header 페이지 flex 조정, contact 메뉴추가
    contact 페이지 추가
    works 페이지 hover 기능 추가

---
+++ src/pages/Contact.js
@@ -0,0 +1,50 @@
+import React from "react";
+import '../css/Contact.css';
+import TextField from '@material-ui/core/TextField';
+
+const Contact = () => {
+    return (
+        <div className="contact-container">
+            <div>
+                <span>영상 문의</span>
+            </div>
+
+            <div className="pt-20">
+                <TextField id="standard-basic" label="이메일" fullWidth={true}/>
+            </div>
+
+            <div className='contact-phone'>
+                <div className="pt-20">
+                    <TextField id="standard-basic" label="전화번호" fullWidth={true}/>
+                </div>
+
+                <div className="pt-20">
+                    <TextField id="standard-basic" label="기업번호" fullWidth={true}/>
+                </div>
+            </div>
+
+            <div className="pt-20">
+                <TextField id="standard-basic" label="이름" fullWidth={true}/>
+            </div>
+
+
+            <div className="pt-20">
+                <TextField
+                    id="outlined-multiline-static"
+                    label="내용"
+                    multiline
+                    rows={10}
+                    variant="outlined"
+                    fullWidth={true}
+                />
+            </div>
+
+
+            <div className="pt-20">
+                <TextField id="standard-basic" label="레퍼런스 영상 및 링크" fullWidth={true}/>
+            </div>
+
+        </div>
+    );
+};
+export default Contact
--- src/pages/What.js
+++ src/pages/What.js
@@ -3,7 +3,7 @@ import React from "react";
 function What() {
     return (
         <div className="flex justify-center">
-            <div>Grigom Pictures 는 영상프로덕션으로서</div>
+
         </div>
     );
 }
