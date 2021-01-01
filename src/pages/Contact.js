0900e8448b2e3c5497e923f942212372a384b6bd
JuHyeonKim07 <rlawngus000407@naver.com>
2021-01-01 20:14:40 +0900

    Contact 기능 개발중

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,50 +1,133 @@
-import React from "react";
+import React, {useEffect, useState, useReducer} from "react";
 import '../css/Contact.css';
-import TextField from '@material-ui/core/TextField';
+import * as mailPoster from "nodemailer";
+import nodeMailer from "nodemailer";
 
-const Contact = () => {
+// 메일 보내는 사람 설정
+nodeMailer.createTestAccount({
+    service: 'worksmobile',
+    host: "smtp.worksmobile.com",
+    port: "465",
+    secure: "true",
+    auth: {
+        user: {
+            user: 'director@grigompictures.com',
+            pass: 'rlaehdgur9107!'
+        }
+    }
+})
+
+// 메일 받은 사람 설정
+const mailOpt = (user_data) => {
+    const mailOptions = {
+        from: '보낼사람',
+        to: 'director@grigompictures.com',
+        subject: '메일제목 입력',
+        text: '내용 입력'
+    };
+    return mailOptions;
+}
+
+// 메일 전송
+const sendMail = (mailOption) => {
+    mailPoster.sendMail(mailOption, function (error, info) {
+        if (error) {
+            console.log('에러 ' + error);
+        } else {
+            console.log('전송 완료 ' + info.response);
+        }
+    });
+}
+
+function reducer(state, {name, value}) {
+    return {
+        ...state,
+        [name]: value,
+    }
+}
+
+function Contact() {
+    const initialState = {
+        email: '',
+        phone: '',
+        number: '',
+        name: '',
+        message: '',
+    }
+
+    // onChange
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const onChange = (event) => {
+        const {name, value} = event.target
+        dispatch({name: name, value})
+    }
+    const {email, phone, number, name, message} = state
+
     return (
-
         <div>
             <div className="title">
                 <span>CONTACT</span>
             </div>
-            <div className="contact-container">
-                <div className="pt-20">
-                    <TextField id="standard-basic" label="이메일" fullWidth={true}/>
-                </div>
 
-                <div className='contact-phone'>
-                    <div className="pt-20">
-                        <TextField id="standard-basic" label="전화번호" fullWidth={true}/>
+            <div className="Contact">
+                <form action="/action_page.php">
+                    <div>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your email"
+                            value={email}
+                            onChange={onChange}
+                            required
+                        />
+
+                        <label>Phone</label>
+                        <input
+                            type="text"
+                            id="phone"
+                            name="phone"
+                            placeholder="Your last name.."
+                            value={phone}
+                            onChange={onChange}
+                            required
+                        />
+
+                        <label>Company Number</label>
+                        <input
+                            type="text"
+                            id="number"
+                            name="number"
+                            placeholder="Your last name.."
+                            value={number}
+                            onChange={onChange}
+                            required
+                        />
+
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Your last name.."
+                            value={name}
+                            onChange={onChange}
+                            required
+                        />
+
+                        <label>Message</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            placeholder="Write something.."
+                            value={message}
+                            onChange={onChange}
+                            required
+                        />
+                        <input type="submit" value="submit"/>
                     </div>
-
-                    <div className="pt-20">
-                        <TextField id="standard-basic" label="기업번호" fullWidth={true}/>
-                    </div>
-                </div>
-
-                <div className="pt-20">
-                    <TextField id="standard-basic" label="이름" fullWidth={true}/>
-                </div>
-
-
-                <div className="pt-20">
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="내용"
-                        multiline
-                        rows={10}
-                        variant="outlined"
-                        fullWidth={true}
-                    />
-                </div>
-
-
-                <div className="pt-20">
-                    <TextField id="standard-basic" label="레퍼런스 영상 및 링크" fullWidth={true}/>
-                </div>
-
+                </form>
             </div>
         </div>
     );
