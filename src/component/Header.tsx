be05935c1e80e3b38199e4dbf2cb0e8b30dee854
KimJuHyeon <rlawngus000407@naver.com>
2022-09-29 23:54:14 +0900

    Header Css 변경 및 Footer 하단 고정 flex 로 변경

---
+++ src/component/Header.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../css/Header.css';
+
+function Header() {
+
+    const onClickLink = () => {
+        const inputCheckElement = document.getElementById('menu-toggle') as HTMLInputElement
+        inputCheckElement.checked = false;
+
+    }
+
+    return (
+        <header>
+            <div className="header-container">
+                <div className="title-div">
+                    <Link to="/">
+                        GRIGOM <br /> PICTURES
+                    </Link>
+                </div>
+
+                <div className="menu-div">
+                    <input id="menu-toggle" name="menu-toggle" type="checkbox" />
+                    <label className="menu-btn" htmlFor="menu-toggle">
+                        <span></span>
+                    </label>
+
+                    <ul className="menu-box">
+                        <li>
+                            <Link to="/" onClick={onClickLink}>ABOUT</Link>
+                        </li>
+                        <li>
+                            <Link to="/Works" onClick={onClickLink}>WORKS</Link>
+                        </li>
+                        <li>
+                            <Link to="/Brands" onClick={onClickLink}>BRANDS</Link>
+                        </li>
+                        <li>
+                            <Link to="/Contact" onClick={onClickLink}>CONTACT</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="service-div">
+                    <div>Vimeo</div>
+                    <div>instagram</div>
+                </div>
+            </div>
+        </header>
+    );
+};
+export default Header
