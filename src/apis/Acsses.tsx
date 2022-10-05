aaf3d96562014bda201ab91b31d152ded233c2ae
EthanKyle <rlawngus000407@naver.com>
2022-10-05 17:32:52 +0900

    redux 기능 구현, 유튜브 api 추가중

---
+++ src/apis/Acsses.tsx
@@ -0,0 +1,19 @@
+type vimeoAcsses_Type = {
+    userid: string,
+    accessToken: string
+}
+
+type youTubeAccses_Type = {
+    apiKey: string
+    playlistId: string
+}
+
+export const vimeoAcsses: vimeoAcsses_Type = {
+    userid: '129526601',
+    accessToken: 'da231ec06f876ff503b91d8152a722a2'
+}
+
+export const youTubeAcsses: youTubeAccses_Type = {
+    apiKey: 'AIzaSyAg2AcqycbLzwK62zaJUzHDGEF5SsEDj90',
+    playlistId: 'PLThDpQpBDLxfDomsur6lq4TQ384K8pStY'
+}
--- src/apis/vimeoAcsses.tsx
+++
@@ -1,9 +0,0 @@
-interface vimeoAcsses_interface {
-    userid: string,
-    accessToken: string
-}
-
-export const vimeoAcsses: vimeoAcsses_interface = {
-    userid: '129526601',
-    accessToken: 'da231ec06f876ff503b91d8152a722a2'
-}
