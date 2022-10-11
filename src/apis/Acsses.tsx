d657fa46695b0cde1d69e53d620bab975be4dc0c
KimJuHyeon <rlawngus000407@naver.com>
2022-10-11 23:16:53 +0900

    ignore 테스트

--- src/apis/Acsses.tsx
+++ src/apis/Acsses.tsx
@@ -8,12 +8,3 @@ type youTubeAccses_Type = {
     playlistId: string
 }
 
-export const vimeoAcsses: vimeoAcsses_Type = {
-    userid: '129526601',
-    accessToken: '5ef3865ea4b2f0f6be84f82a2db29ed2'
-}
-
-export const youTubeAcsses: youTubeAccses_Type = {
-    apiKey: 'AIzaSyCy4FTRTkXja0rlP29MpCzdeQnZethNBf8',
-    playlistId: 'PLThDpQpBDLxfDomsur6lq4TQ384K8pStY'
-}
