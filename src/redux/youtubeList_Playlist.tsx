b3f5effcc132c0fd1473136c0b8fa32fbea60c84
EthanKyle <rlawngus000407@naver.com>
2022-11-01 17:48:31 +0900

    extraReducers 에서 데이터 병합 기능 추가

--- src/redux/youtubeList_Playlist.tsx
+++ src/redux/youtubeList_Playlist.tsx
@@ -10,21 +10,23 @@ interface PostState {
     loading: boolean;
     error: string | null;
     data: youtubeResponse | null;
+    cartegory: {}[]
 }
 
 const initialState = {
     loading: false,
     error: null,
     data: null,
+    cartegory: []
 } as PostState;
 
 // ACTION
 export const youtubeList_Playlist = createAsyncThunk(
     "GET/YOUTUBE_CHANNELID",
-    async (arg : string, thunkAPI) => {
+    async (channelId: string, thunkAPI) => {
         try {
             const { data } = await axios.get<youtubeResponse>(
-                `https://www.googleapis.com/youtube/v3/playlists?key=${youTubeAcsses.apiKey}&channelId=${arg}&part=snippet&maxResults=30`
+                `https://www.googleapis.com/youtube/v3/playlists?key=${youTubeAcsses.apiKey}&channelId=${channelId}&part=snippet&maxResults=30`
                 // channelId=UCvpIHsNLXfpOj_uMgI62I2A - 그리곰 픽쳐스 상업 유튜브 채널
             )
             return data
@@ -50,6 +52,7 @@ const youtube_PlaylistSlice = createSlice({
             .addCase(youtubeList_Playlist.fulfilled, (state, action: PayloadAction<youtubeResponse>) => {
                 state.loading = false;
                 state.data = action.payload;
+                state.cartegory = [...state.data?.items, ...action.payload.items];
             })
             .addCase(youtubeList_Playlist.rejected, (state, action: PayloadAction<any>) => {
                 state.error = action.payload;
