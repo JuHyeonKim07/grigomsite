cd8aba2cf7c05f8b81cc715686bd32a134b0e518
KimJuHyeon <rlawngus000407@naver.com>
2023-01-04 22:53:47 +0900

    주석 코드 제거

--- src/component/YoutubeTab.tsx
+++ src/component/YoutubeTab.tsx
@@ -31,12 +31,6 @@ function YoutubeTab({ playlistId }: propsTypes) {
                             return (
                                 <div className="image" key={index}>
                                     <img className="image__img" src={value.snippet.thumbnails.maxres ? value.snippet.thumbnails.maxres.url : value.snippet.thumbnails.high.url} alt='bricks' />
-                                    {/* <Link to={`/Details/${encodeURIComponent(
-                                        `<iframe id="player" type="text/html" 
-                                        src="https://www.youtube.com/embed/${value.snippet.resourceId.videoId}"
-                                        frameborder="0"></iframe>`
-                                    )}`}> */}
-
                                     <Link to={`/Details/${value.snippet.resourceId.videoId}`}>
                                         <div className="image__overlay image__overlay--youtube">
                                             <div className="image__title">Grigom Pictures</div>
